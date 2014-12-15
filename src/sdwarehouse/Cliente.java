a3d56bab747f7b9ba0a07de26984cdb0077e7c18
Jose <Jose@172.19.106.212>
2014-12-15 16:45:23 +0000

    Esqueleto das classes

---
+++ src/sdwarehouse/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sdwarehouse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Jose
+ */
+public class Cliente 
+{
+    private String nome, password;
+    private HashMap<String, Tarefa> tarefas;
+    private HashMap<String, TarefaExecucao> tarefasExec;
+    
+}
---
+++ src/sdwarehouse/ServeState.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sdwarehouse;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Jose
+ */
+public class ServeState 
+{
+    private HashMap<String, Cliente> clientes;
+    private WareHouse armazem;
+}
