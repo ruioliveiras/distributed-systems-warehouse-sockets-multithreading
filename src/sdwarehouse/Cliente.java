e79435b6e5e4c37e3a7d0857af84fb19441ae247
User <User@User-PC>
2014-12-16 10:11:45 +0000

    Construtores/get Cliente

--- src/sdwarehouse/Cliente.java
+++ src/sdwarehouse/Cliente.java
@@ -17,5 +17,35 @@ public class Cliente
     private String nome, password;
     private HashMap<String, Tarefa> tarefas;
     private HashMap<String, TarefaExecucao> tarefasExec;
+
+    public Cliente() {
+        this.nome = "";
+        this.password = "";
+        this.tarefas = new HashMap<>();
+        this.tarefasExec = new HashMap<>();
+    }
+    
+    public Cliente(String nome, String password) {
+        this.nome = nome;
+        this.password = password;
+        this.tarefas = new HashMap<>();
+        this.tarefasExec = new HashMap<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public HashMap<String, Tarefa> getTarefas() {
+        return tarefas;
+    }
+
+    public HashMap<String, TarefaExecucao> getTarefasExec() {
+        return tarefasExec;
+    }
     
 }
