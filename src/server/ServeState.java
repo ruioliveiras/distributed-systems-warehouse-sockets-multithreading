dc3e151f61c13dbc99a3db79b7d566658a3f2bd1
Rui Oliveiras <rui96pedro@hotmail.com>
2014-12-22 16:01:12 +0000

    Some random edition

--- src/server/ServeState.java
+++ src/server/ServeState.java
@@ -1,9 +1,9 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template fi   le, choose Tools | Templates
  * and open the template in the editor.
  */
-
+    
 package server;
 
 import server.model.WareHouse;
@@ -18,4 +18,6 @@ public class ServeState
 {
     private HashMap<String, Cliente> clientes;
     private WareHouse armazem;
+    
+   
 }
--- src/shared/Facede.java
+++ src/shared/Facede.java
@@ -15,20 +15,66 @@ package shared;
 public interface Facede {
     
     /*Authentication*/
+    /**
+     * 
+     * @param username String with the username
+     * @param password String with the password
+     * @return True if is logged, False if is not logged
+     * @throws SimpleExecption 
+     */
     public Boolean login(String username, String password) throws SimpleExecption;
    
     /* FOR USER */
+    /**
+     * 
+     * @param username String with the username
+     * @param password String with the password
+     * @return True if success, false if it is not
+     * @throws SimpleExecption 
+     */
     public Boolean addUser(String username, String password) throws SimpleExecption;
    
+    /**
+     * 
+     * @param username String with the username
+     * @param password String with the password
+     * @return return true if succes, false if it is not
+     * @throws SimpleExecption 
+     */
     public Boolean editUser(String username, String password) throws SimpleExecption;
-
+    
+    /**
+     * Return 
+     * @param username String with the username
+     * @return the list of all users
+     * @throws SimpleExecption 
+     */
     public String[] listUser(String username) throws SimpleExecption;
     
     /*FOR WAREHOUSE*/
+    /**
+     * Create new Object with Zero quantiy
+     * @param nome
+     * @return Success boolean
+     * @throws SimpleExecption 
+     */
     public Boolean addObj(String nome) throws SimpleExecption;
 
+    /**
+     * Delete an Object
+     * @param nome name of the object
+     * @return Success boolean
+     * @throws SimpleExecption 
+     */
     public Boolean remObj(String nome) throws SimpleExecption;
 
+    /**
+     * Add more items 
+     * @param nome
+     * @param quantidade
+     * @return
+     * @throws SimpleExecption 
+     */
     public Boolean supplyObj(String nome, int quantidade) throws SimpleExecption;
 
     public String[] listObj() throws SimpleExecption;
